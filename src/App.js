3110b62a7d1a835d6ecd0900fee067aaaf52defb
arias-ds3 <ariasfa7@gmail.com>
2025-01-15 11:13:57 -0300

    show list of task

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import { useState , useRef, useEffect } from 'react';
+
+function App() {
+  
+  let [task, setTask] = useState([
+    {
+      name: "Task1",
+      place: "School",
+      priority: 0
+    },
+    {
+      name: "Clean",
+      place: "House",
+      priority: 0
+    },
+  ])
+
+  return ( 
+    <div>
+        <ul>
+        { task.map( t =>
+          <li>
+            <b>{t.name}</b>
+            <div>Priority: {t.priority}</div>
+            <div> {t.place} </div>
+          </li>) }
+          </ul>
+      </div>
+
+  )
+  ;
+}
+
+export default App;
