be94ed6a1727fba7caabd5b386a67e5896b60b24
arias-ds3 <ariasfa7@gmail.com>
2025-01-24 10:29:47 -0300

    create useref

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ let App = () => {
   let [place, setPlace] = useState("");
   let [priority, setPriority] = useState(0);
 
+  let nameRef = useRef("");
+  let placeRef = useRef("");
+  let priorityRef = useRef(0);
+
+
   let [tasks, setTasks] = useState([
     {
       name: "Task1",
@@ -27,6 +32,15 @@ let addTaks = () => {
   }
   setTasks([...tasks, newTask])
 }
+
+let addTaksWithRefs = () => {
+  let newTask = {
+    name: nameRef.current.value,
+    place: placeRef.current.value,
+    priority: priorityRef.current.value
+  }
+  setTasks([...tasks, newTask])
+}
   
   return (
     <div>
@@ -36,6 +50,11 @@ let addTaks = () => {
       <input type='number' placeholder='priority' onChange={(e) => {setPriority(parseInt)(e.currentTarget.value)}}></input>
        <button onClick={addTaks}>ADD Task</button>
 
+      <h2>Add task ref</h2>
+      <input ref={nameRef} type='text' placeholder='name' ></input>
+      <input ref={placeRef} type='text' placeholder='place'></input>
+      <input ref={priorityRef} type='number' placeholder='priority'></input>
+       <button onClick={addTaksWithRefs}>ADD Task</button>
 
       <ul>
         { tasks.map( t => 
