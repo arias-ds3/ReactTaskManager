7253e5964603baa8c997dd82cc37de80cb9689d1
arias-ds3 <ariasfa7@gmail.com>
2025-01-24 11:08:01 -0300

    details task component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import './App.css';
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef } from 'react';
+import DetailOfTasksComponent from './Components/DetailsOfTasksComponent';
 
 let App = () => {
   let [name, setName] = useState("");
   let [place, setPlace] = useState("");
   let [priority, setPriority] = useState(0);
-
-  let nameRef = useRef("");
-  let placeRef = useRef("");
-  let priorityRef = useRef(0);
-
-  let formRef = useRef()
 
   let [tasks, setTasks] = useState([
     {
@@ -37,58 +32,24 @@ let addTaks = () => {
   setPriority("")
 }
 
-let addTaksWithRefs = () => {
-  let newTask = {
-    name: nameRef.current.value,
-    place: placeRef.current.value,
-    priority: priorityRef.current.value
-  }
-  setTasks([...tasks, newTask])
-  nameRef.current.value = "";
-  placeRef.current.value = "";
-  priorityRef.current.value = ""
-}
-let addTaksWithformRefs = () => {
-  let formData = new FormData(formRef.current);
-
-  let newTask = {
-    name: formData.get("name"),
-    place: formData.get("place"),
-    priority: formData.get("priority")
-  }
-  setTasks([...tasks, newTask])
-}
   
   return (
-    <div>
+    <div className='container'>
+      <h1>List of task</h1>
+      <ul className='task-list'>
+        { tasks.map( t => 
+          <li key={ t.name}>{ t.name}  </li> )}
+      </ul>
+
+      <div className='task-form'>
       <h2>Add tasks</h2>
       <input type='text' placeholder='name' value={name} onChange={(e) => {setName(e.currentTarget.value)}}></input>
       <input type='text' placeholder='place' value={place} onChange={(e) => {setPlace(e.currentTarget.value)}}></input>
       <input type='number' placeholder='priority' value={priority} onChange={(e) => {setPriority(parseInt)(e.currentTarget.value)}}></input>
-       <button onClick={addTaks}>ADD Task</button>
+      <button onClick={addTaks}>Add Task</button>
+      </div>
 
-      <h2>Add task ref</h2>
-      <input ref={nameRef} type='text' placeholder='name' ></input>
-      <input ref={placeRef} type='text' placeholder='place'></input>
-      <input ref={priorityRef} type='number' placeholder='priority'></input>
-       <button onClick={addTaksWithRefs}>ADD Task</button>
-
-       <h2>Add task form ref</h2>
-       <form ref={formRef}>
-          <input name='name' type='text' placeholder='name' ></input>
-          <input name='place' type='text' placeholder='place'></input>
-          <input name='priority' type='number' placeholder='priority'></input>
-        </form>
-       <button onClick={addTaksWithformRefs}>ADD Task</button>
-
-      <ul>
-        { tasks.map( t => 
-          <li>
-            <b>{ t.name} </b>
-            <div>Priority: {t.priority} </div>
-            <div>{t.place} </div>
-          </li> )}
-      </ul>
+      <DetailOfTasksComponent/>
     </div>
   )
   ;
